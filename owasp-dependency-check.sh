9b596137b709d40331d5ad14c076388ed93652dc
Nitika-cloud <69337804+Nitika-cloud@users.noreply.github.com>
2021-06-29 13:56:32 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -14,16 +14,16 @@ if [ ! -d "$DATA_DIRECTORY" ]; then
 fi
 
 # Make sure we are using the latest version
-docker pull owasp/dependency-check
-
-docker run --rm \
-    --volume $(pwd):/src \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
-    --volume "$REPORT_DIRECTORY":/report \
-    owasp/dependency-check \
-    --scan /src \
-    --format "ALL" \
-    --project "My OWASP Dependency Check Project" \
-    --out /report
+docker pull owasp/dependency-check# test
+                                   
+#docker run --rm \
+#    --volume $(pwd):/src \
+#    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+#    --volume "$REPORT_DIRECTORY":/report \
+ #   owasp/dependency-check \
+  #  --scan /src \
+   # --format "ALL" \
+    #--project "My OWASP Dependency Check Project" \
+    #--out /report
     # Use suppression like this: (/src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
